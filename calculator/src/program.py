e19d7304ca80e3df85461fc63c349f718d601474
carolinarigaudfelix <carolinafelix.dev@gmail.com>
2023-04-09 20:46:36 -0300

    to-fix-display

--- calculator/src/program.py
+++ calculator/src/program.py
@@ -7,7 +7,6 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__()
         self.setupUi(self)
 
-        lista = []    
         def calc(arg):
            if arg == None:
             self.visor.display('')
@@ -17,27 +16,69 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         # Conecta os botões aos slots
 
-        self.btn_0.clicked.connect(lambda: calc(self.btn_0.text()))
-        self.btn_1.clicked.connect(lambda: calc(self.btn_1.text()))
-        self.btn_2.clicked.connect(lambda: calc(self.btn_2.text()))
-        self.btn_3.clicked.connect(lambda: calc(self.btn_3.text()))
-        self.btn_4.clicked.connect(lambda: calc(self.btn_4.text()))
-        self.btn_5.clicked.connect(lambda: calc(self.btn_5.text()))
-        self.btn_6.clicked.connect(lambda: calc(self.btn_6.text()))
-        self.btn_7.clicked.connect(lambda: calc(self.btn_7.text()))
-        self.btn_8.clicked.connect(lambda: calc(self.btn_8.text()))
-        self.btn_9.clicked.connect(lambda: calc(self.btn_9.text()))
-        self.btn_addition.clicked.connect(lambda: calc(self.btn_addition.text()))
-        self.btn_subtraction.clicked.connect(lambda: calc(self.btn_subtraction.text()))
-        self.btn_multiplication.clicked.connect(lambda: calc(self.btn_multiplication.text()))
-        self.btn_potentiation.clicked.connect(lambda: calc(self.btn_potentiation.text()))
-        self.btn_result.clicked.connect(lambda: calc(self.btn_result.text()))
-        self.btn_clear.clicked.connect(lambda: calc(self.btn_clear.text()))
-        self.btn_delete_2.clicked.connect(lambda: calc(self.btn_delete2.text()))
-        self.btn_dot.clicked.connect(lambda: calc(self.btn_dot.text()))
+        self.pushButton_0.clicked.connect(self.adicionar_numero)
+        self.pushButton_1.clicked.connect(self.adicionar_numero)
+        self.pushButton_2.clicked.connect(self.adicionar_numero)
+        self.pushButton_3.clicked.connect(self.adicionar_numero)
+        self.pushButton_4.clicked.connect(self.adicionar_numero)
+        self.pushButton_5.clicked.connect(self.adicionar_numero)
+        self.pushButton_6.clicked.connect(self.adicionar_numero)
+        self.pushButton_7.clicked.connect(self.adicionar_numero)
+        self.pushButton_8.clicked.connect(self.adicionar_numero)
+        self.pushButton_9.clicked.connect(self.adicionar_numero)
+        self.pushButton_add.clicked.connect(self.adicionar_operador)
+        self.pushButton_subtract.clicked.connect(self.adicionar_operador)
+        self.pushButton_multiply.clicked.connect(self.adicionar_operador)
+        self.pushButton_divide.clicked.connect(self.adicionar_operador)
+        # self.btn_potentiation.clicked.connect(lambda: self.operator_pressed('^'))
+        self.pushButton_equals.clicked.connect(self.calcular)
+        self.pushButton_clear.clicked.connect(self.clear_pressed)
+
+        self.num1 = ""
+        self.num2 = ""
+        self.operador = ""
+
+
+    def adicionar_numero(self):
+        numero = self.sender().text()
+        if self.operador == "":
+            self.num1 += numero
+            self.visor.display(self.num1)
+        else:
+            self.num2 += numero
+            self.visor.display(self.num2)
+
+    # def number_pressed(self, digit):
+    #     self.current_number += digit
+    #     self.visor.setText(self.current_number)
+
+    def adicionar_operador(self):
+           self.operador = self.sender().text()
+           self.pushButton_equals.setText("")
+
+    def calcular(self):
+           if self.num1 != "" and self.num2 != "":
+               if self.operador == "+":
+                   resultado = float(self.num1) + float(self.num2)
+               elif self.operador == "-":
+                   resultado = float(self.num1) - float(self.num2)
+               elif self.operador == "*":
+                   resultado = float(self.num1) * float(self.num2)
+               elif self.operador == "/":
+                   resultado = float(self.num1) / float(self.num2)
+               self.visor.display(str(resultado))
+               self.num1 = str(resultado)
+               self.num2 = ""
+               self.operador = ""
+
+    def clear_pressed(self):
+        self.result = 0
+        self.operation = ''
+        self.current_number = ''
+        self.visor.setText('')
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- calculator/src/window.py
+++ calculator/src/window.py
@@ -51,151 +51,151 @@ class Ui_MainWindow(object):
 
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
-        self.btn_result = QPushButton(self.frame)
-        self.btn_result.setObjectName(u"btn_result")
+        self.pushButton_equals = QPushButton(self.frame)
+        self.pushButton_equals.setObjectName(u"pushButton_equals")
         font1 = QFont()
         font1.setPointSize(12)
-        self.btn_result.setFont(font1)
-        self.btn_result.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_equals.setFont(font1)
+        self.pushButton_equals.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_result, 8, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_equals, 8, 4, 1, 1)
 
-        self.btn_4 = QPushButton(self.frame)
-        self.btn_4.setObjectName(u"btn_4")
-        self.btn_4.setFont(font1)
-        self.btn_4.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_4 = QPushButton(self.frame)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setFont(font1)
+        self.pushButton_4.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_4, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_4, 6, 0, 1, 1)
 
-        self.btn_3 = QPushButton(self.frame)
-        self.btn_3.setObjectName(u"btn_3")
-        self.btn_3.setFont(font1)
-        self.btn_3.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_3 = QPushButton(self.frame)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setFont(font1)
+        self.pushButton_3.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_3, 7, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_3, 7, 2, 1, 1)
 
-        self.btn_division = QPushButton(self.frame)
-        self.btn_division.setObjectName(u"btn_division")
-        self.btn_division.setFont(font1)
-        self.btn_division.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_divide = QPushButton(self.frame)
+        self.pushButton_divide.setObjectName(u"pushButton_divide")
+        self.pushButton_divide.setFont(font1)
+        self.pushButton_divide.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_division, 2, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_divide, 2, 4, 1, 1)
 
-        self.btn_5 = QPushButton(self.frame)
-        self.btn_5.setObjectName(u"btn_5")
-        self.btn_5.setFont(font1)
-        self.btn_5.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_5 = QPushButton(self.frame)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setFont(font1)
+        self.pushButton_5.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_5, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_5, 6, 1, 1, 1)
 
-        self.btn_addition = QPushButton(self.frame)
-        self.btn_addition.setObjectName(u"btn_addition")
-        self.btn_addition.setFont(font1)
-        self.btn_addition.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_add = QPushButton(self.frame)
+        self.pushButton_add.setObjectName(u"pushButton_add")
+        self.pushButton_add.setFont(font1)
+        self.pushButton_add.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_addition, 7, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_add, 7, 4, 1, 1)
 
-        self.btn_6 = QPushButton(self.frame)
-        self.btn_6.setObjectName(u"btn_6")
-        self.btn_6.setFont(font1)
-        self.btn_6.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_6 = QPushButton(self.frame)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setFont(font1)
+        self.pushButton_6.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_6, 6, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_6, 6, 2, 1, 1)
 
-        self.btn_multiplication = QPushButton(self.frame)
-        self.btn_multiplication.setObjectName(u"btn_multiplication")
-        self.btn_multiplication.setFont(font1)
-        self.btn_multiplication.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_multiply = QPushButton(self.frame)
+        self.pushButton_multiply.setObjectName(u"pushButton_multiply")
+        self.pushButton_multiply.setFont(font1)
+        self.pushButton_multiply.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_multiplication, 4, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_multiply, 4, 4, 1, 1)
 
-        self.btn_2 = QPushButton(self.frame)
-        self.btn_2.setObjectName(u"btn_2")
-        self.btn_2.setFont(font1)
-        self.btn_2.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_2 = QPushButton(self.frame)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setFont(font1)
+        self.pushButton_2.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_2, 7, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_2, 7, 1, 1, 1)
 
-        self.btn_0 = QPushButton(self.frame)
-        self.btn_0.setObjectName(u"btn_0")
-        self.btn_0.setFont(font1)
-        self.btn_0.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_0 = QPushButton(self.frame)
+        self.pushButton_0.setObjectName(u"pushButton_0")
+        self.pushButton_0.setFont(font1)
+        self.pushButton_0.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_0, 8, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_0, 8, 1, 1, 1)
 
-        self.btn_1 = QPushButton(self.frame)
-        self.btn_1.setObjectName(u"btn_1")
-        self.btn_1.setFont(font1)
-        self.btn_1.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_1 = QPushButton(self.frame)
+        self.pushButton_1.setObjectName(u"pushButton_1")
+        self.pushButton_1.setFont(font1)
+        self.pushButton_1.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_1, 7, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_1, 7, 0, 1, 1)
 
-        self.btn_dot = QPushButton(self.frame)
-        self.btn_dot.setObjectName(u"btn_dot")
-        self.btn_dot.setFont(font1)
-        self.btn_dot.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_dot = QPushButton(self.frame)
+        self.pushButton_dot.setObjectName(u"pushButton_dot")
+        self.pushButton_dot.setFont(font1)
+        self.pushButton_dot.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_dot, 8, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_dot, 8, 2, 1, 1)
 
-        self.btn_8 = QPushButton(self.frame)
-        self.btn_8.setObjectName(u"btn_8")
-        self.btn_8.setFont(font1)
-        self.btn_8.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_8 = QPushButton(self.frame)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setFont(font1)
+        self.pushButton_8.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_8, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_8, 4, 1, 1, 1)
 
-        self.btn_subtraction = QPushButton(self.frame)
-        self.btn_subtraction.setObjectName(u"btn_subtraction")
-        self.btn_subtraction.setFont(font1)
-        self.btn_subtraction.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_subtract = QPushButton(self.frame)
+        self.pushButton_subtract.setObjectName(u"pushButton_subtract")
+        self.pushButton_subtract.setFont(font1)
+        self.pushButton_subtract.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_subtraction, 6, 4, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_subtract, 6, 4, 1, 1)
 
-        self.btn_7 = QPushButton(self.frame)
-        self.btn_7.setObjectName(u"btn_7")
-        self.btn_7.setFont(font1)
-        self.btn_7.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_7 = QPushButton(self.frame)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setFont(font1)
+        self.pushButton_7.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_7, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_7, 4, 0, 1, 1)
 
-        self.btn_9 = QPushButton(self.frame)
-        self.btn_9.setObjectName(u"btn_9")
-        self.btn_9.setFont(font1)
-        self.btn_9.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
+        self.pushButton_9 = QPushButton(self.frame)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setFont(font1)
+        self.pushButton_9.setStyleSheet(u"background-color: rgb(157, 195, 255);\n"
 "color: rgb(255, 255, 255);")
 
-        self.gridLayout.addWidget(self.btn_9, 4, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_9, 4, 2, 1, 1)
 
-        self.btn_clear = QPushButton(self.frame)
-        self.btn_clear.setObjectName(u"btn_clear")
-        self.btn_clear.setFont(font1)
-        self.btn_clear.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_clear = QPushButton(self.frame)
+        self.pushButton_clear.setObjectName(u"pushButton_clear")
+        self.pushButton_clear.setFont(font1)
+        self.pushButton_clear.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_clear, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_clear, 2, 0, 1, 1)
 
-        self.btn_delete_2 = QPushButton(self.frame)
-        self.btn_delete_2.setObjectName(u"btn_delete_2")
-        self.btn_delete_2.setFont(font1)
-        self.btn_delete_2.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_delete = QPushButton(self.frame)
+        self.pushButton_delete.setObjectName(u"pushButton_delete")
+        self.pushButton_delete.setFont(font1)
+        self.pushButton_delete.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_delete_2, 2, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_delete, 2, 1, 1, 1)
 
-        self.btn_potentiation = QPushButton(self.frame)
-        self.btn_potentiation.setObjectName(u"btn_potentiation")
-        self.btn_potentiation.setFont(font1)
-        self.btn_potentiation.setStyleSheet(u"background: rgb(85, 170, 255);")
+        self.pushButton_potentiation = QPushButton(self.frame)
+        self.pushButton_potentiation.setObjectName(u"pushButton_potentiation")
+        self.pushButton_potentiation.setFont(font1)
+        self.pushButton_potentiation.setStyleSheet(u"background: rgb(85, 170, 255);")
 
-        self.gridLayout.addWidget(self.btn_potentiation, 2, 2, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_potentiation, 2, 2, 1, 1)
 
 
         self.gridLayout_2.addLayout(self.gridLayout, 1, 1, 1, 1)
@@ -209,7 +209,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 360, 22))
+        self.menubar.setGeometry(QRect(0, 0, 360, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -222,24 +222,24 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.btn_result.setText(QCoreApplication.translate("MainWindow", u"=", None))
-        self.btn_4.setText(QCoreApplication.translate("MainWindow", u"4", None))
-        self.btn_3.setText(QCoreApplication.translate("MainWindow", u"3", None))
-        self.btn_division.setText(QCoreApplication.translate("MainWindow", u"/", None))
-        self.btn_5.setText(QCoreApplication.translate("MainWindow", u"5", None))
-        self.btn_addition.setText(QCoreApplication.translate("MainWindow", u"+", None))
-        self.btn_6.setText(QCoreApplication.translate("MainWindow", u"6", None))
-        self.btn_multiplication.setText(QCoreApplication.translate("MainWindow", u"*", None))
-        self.btn_2.setText(QCoreApplication.translate("MainWindow", u"2", None))
-        self.btn_0.setText(QCoreApplication.translate("MainWindow", u"0", None))
-        self.btn_1.setText(QCoreApplication.translate("MainWindow", u"1", None))
-        self.btn_dot.setText(QCoreApplication.translate("MainWindow", u".", None))
-        self.btn_8.setText(QCoreApplication.translate("MainWindow", u"8", None))
-        self.btn_subtraction.setText(QCoreApplication.translate("MainWindow", u"-", None))
-        self.btn_7.setText(QCoreApplication.translate("MainWindow", u"7", None))
-        self.btn_9.setText(QCoreApplication.translate("MainWindow", u"9", None))
-        self.btn_clear.setText(QCoreApplication.translate("MainWindow", u"C", None))
-        self.btn_delete_2.setText(QCoreApplication.translate("MainWindow", u"D", None))
-        self.btn_potentiation.setText(QCoreApplication.translate("MainWindow", u"^", None))
+        self.pushButton_equals.setText(QCoreApplication.translate("MainWindow", u"=", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"4", None))
+        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"3", None))
+        self.pushButton_divide.setText(QCoreApplication.translate("MainWindow", u"/", None))
+        self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"5", None))
+        self.pushButton_add.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"6", None))
+        self.pushButton_multiply.setText(QCoreApplication.translate("MainWindow", u"*", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"2", None))
+        self.pushButton_0.setText(QCoreApplication.translate("MainWindow", u"0", None))
+        self.pushButton_1.setText(QCoreApplication.translate("MainWindow", u"1", None))
+        self.pushButton_dot.setText(QCoreApplication.translate("MainWindow", u".", None))
+        self.pushButton_8.setText(QCoreApplication.translate("MainWindow", u"8", None))
+        self.pushButton_subtract.setText(QCoreApplication.translate("MainWindow", u"-", None))
+        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"7", None))
+        self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"9", None))
+        self.pushButton_clear.setText(QCoreApplication.translate("MainWindow", u"C", None))
+        self.pushButton_delete.setText(QCoreApplication.translate("MainWindow", u"D", None))
+        self.pushButton_potentiation.setText(QCoreApplication.translate("MainWindow", u"^", None))
     # retranslateUi
 
